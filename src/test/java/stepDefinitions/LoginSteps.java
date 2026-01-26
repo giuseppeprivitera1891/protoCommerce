8fad2edf983f6d446c801740ab05418881399c6e
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-26 12:48:08 +0100

    Added steps for first And on LoginSteps java class

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,9 +1,12 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import utility.DriverManager;
 
 public class LoginSteps {
@@ -18,5 +21,15 @@ public class LoginSteps {
     public void the_user_enters_valid_username_and_password (String username, String password) {
         driver.findElement(By.id("username")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    @And("selects the type of user and accept the terms")
+    public void selects_the_type_of_user_and_accept_the_terms() {
+        WebElement userRadioButton = driver.findElement(By.cssSelector("input[value='user']"));
+        userRadioButton.click();
+        Assert.assertTrue(userRadioButton.isSelected());
+        WebElement termCheckbox = driver.findElement(By.id("terms"));
+        termCheckbox.click();
+        Assert.assertTrue(termCheckbox.isSelected());
     }
 }
