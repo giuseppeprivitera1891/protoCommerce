f682f07ecf1fd9ac260c80002d159768169bbb97
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-26 12:06:53 +0100

    Code on Hooks Java class

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,4 +1,17 @@
 package stepDefinitions;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
+import utility.DriverManager;
+
 public class Hooks {
+    @BeforeAll
+    public static void beforeAll() {
+        DriverManager.initDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        DriverManager.quitDriver();
+    }
 }
