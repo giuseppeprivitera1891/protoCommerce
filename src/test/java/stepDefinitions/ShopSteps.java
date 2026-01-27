4fecce866955ca8718bc468689167a1adf780a62
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-27 12:44:35 +0100

    Added explicit wait to see a product card on the shop page for Given on ShopSteps

--- src/test/java/stepDefinitions/ShopSteps.java
+++ src/test/java/stepDefinitions/ShopSteps.java
@@ -5,9 +5,12 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import utility.DriverManager;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,18 +23,20 @@ public class ShopSteps {
         String urlShopPage = driver.getCurrentUrl();
         System.out.println("The URL of shop page is " + urlShopPage);
         Assert.assertTrue(urlShopPage.contains(shopPage));
+        WebDriverWait waitProduct = new WebDriverWait(driver, Duration.ofSeconds(2));
+        waitProduct.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='card h-100']")));
     }
 
     @When("the user adds the products")
     public void the_user_adds_the_products() {
-        List<WebElement> products = driver.findElements(By.cssSelector("card h-100"));
+        List<WebElement> products = driver.findElements(By.xpath("//div[@class='card h-100']"));
         WebElement addButton = driver.findElement(By.xpath("//button[@class='btn btn-info'][normalize-space()='Add']"));
 
         for (String addProduct : myProduct) {
             for (WebElement product : products) {
                 String productName = product.findElement(By.cssSelector("h4[class='card-title']")).getText();
                 if (productName.equalsIgnoreCase(addProduct)) {
-                    addButton.click();
+                    product.findElement(By.cssSelector(".card-footer button")).click();
                     break;
                 }
             }
