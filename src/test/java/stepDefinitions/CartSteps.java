a7559ebc7cd302d22095cc081635ed0387992465
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-28 10:44:27 +0100

    Added When method on CartSteps Java class

---
+++ src/test/java/stepDefinitions/CartSteps.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utility.DriverManager;
+
+public class CartSteps {
+    private final WebDriver driver = DriverManager.getDriver();
+
+    @When("the user adds the quantity for a product")
+    public void the_user_adds_the_quantity_for_a_product() {
+        WebElement firstProductQuantity = driver.findElement(By.id("exampleInputEmail1"));
+        firstProductQuantity.sendKeys("3");
+    }
+
+
+}
