a0337552eba7905c52f9afa22a3e8fb70cc918e8
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-28 10:45:57 +0100

    Edited When method on CartSteps Java class

--- src/test/java/stepDefinitions/CartSteps.java
+++ src/test/java/stepDefinitions/CartSteps.java
@@ -2,6 +2,7 @@ package stepDefinitions;
 
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utility.DriverManager;
@@ -12,6 +13,7 @@ public class CartSteps {
     @When("the user adds the quantity for a product")
     public void the_user_adds_the_quantity_for_a_product() {
         WebElement firstProductQuantity = driver.findElement(By.id("exampleInputEmail1"));
+        firstProductQuantity.sendKeys(Keys.BACK_SPACE);
         firstProductQuantity.sendKeys("3");
     }
 
