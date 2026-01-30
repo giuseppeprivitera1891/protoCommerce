2a43cb963ca382ce842498790416d4ccdb7d3834
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-01-30 12:41:21 +0100

    Added callWaitTitle method on GenericUtils.java

--- src/test/java/utility/GenericUtils.java
+++ src/test/java/utility/GenericUtils.java
@@ -2,7 +2,6 @@ package utility;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -26,6 +25,11 @@ public class GenericUtils {
         wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
         wait.pollingEvery(Duration.ofSeconds(pollingEvery));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void callWaitTitle(int seconds, String title) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.titleIs(title));
     }
 
 }
