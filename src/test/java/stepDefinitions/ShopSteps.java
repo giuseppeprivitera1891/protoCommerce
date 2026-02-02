6d6ecbb43d54c5ab8b106d8be0913bd27ed625a9
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-02-02 12:33:20 +0100

    Added method for Then step, removed unused libraries and a variable on ShopSteps.java

--- src/test/java/stepDefinitions/ShopSteps.java
+++ src/test/java/stepDefinitions/ShopSteps.java
@@ -4,20 +4,10 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import pageObjects.ShopPage;
-
-import java.time.Duration;
 
 public class ShopSteps {
     ShopPage shopPage = new ShopPage();
-
-    String expectedProductTableHead = "Product";
-    int twoSeconds = 2;
 
     @Given("the user is on the {string}")
     public void the_user_is_on_the_shopPage(String urlShopPage) {
@@ -36,9 +26,6 @@ public class ShopSteps {
 
     @Then("the user should see the cart page")
     public void the_user_should_see_the_cart_page() {
-        String actualProductTableHead = driver.findElement(By.xpath("//th[normalize-space()='Product']")).getText();
-        WebDriverWait waitTable = new WebDriverWait(driver, Duration.ofSeconds(2));
-        waitTable.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[normalize-space()='Product']")));
-        Assert.assertEquals(actualProductTableHead, expectedProductTableHead);
+        shopPage.theUserShouldSeeTheCartPage();
     }
 }
