8baaecf4221091f92fbda8a8167ed7356a2f9ea8
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-02-03 11:52:09 +0100

    Added method for When step to add the quantity for a product on ShopPage.java

--- src/test/java/pageObjects/ShopPage.java
+++ src/test/java/pageObjects/ShopPage.java
@@ -21,18 +21,34 @@ public class ShopPage {
     String expectedCheckoutTextButton = "Checkout ( 2 )\n(current)";
     String actualProductTableHead;
     String expectedProductTableHead = "Product";
+    String getFirstProductText;
+    String expectedFirstProductText = "iphone X";
+    String expectedUnitPriceFirstProduct = "₹. 100000";
+    String expectedTotalPriceFirstProduct = "₹. 300000";
+    String expectedSecondProductQuantity = "1";
+    String expectedUnitPriceSecondProduct = "₹. 65000";
+    String expectedTotalPriceSecondProduct = "₹. 65000";
+    String expectedTotalPrice = "₹. 365000";
+    String expectedCheckoutFinalTextButton = "Checkout";
+    String expectedDeliveryText = "Please choose your delivery location.\n" +
+            "Then click on purchase button ";
+    String sendFirstFirstQuantity = "3";
     int  twoSeconds = 2;
     int fiveSeconds = 5;
+    int tenSeconds = 10;
     int thirtySeconds = 30;
 
     List<WebElement> products;
     WebElement checkoutButton;
+    WebElement firstProductQuantity;
 
     By cardProducts = By.xpath("//div[@class='card h-100']");
     By cardTitle = By.cssSelector("h4[class='card-title']");
     By cardFooterButton = By.cssSelector(".card-footer button");
     By actualTextCheckoutButton = By.xpath("(//a[@class='nav-link btn btn-primary'])[1]");
     By actualTextProductTableHead = By.xpath("//th[normalize-space()='Product']");
+    By firstProductText = By.xpath("//a[normalize-space()='iphone X'][1]");
+    By getFirstProductQuantity = By.xpath("//a[normalize-space()='iphone X']/ancestor::tr//input");
 
     public void theUserIsOnTheShopPage(String url) {
         urlShopPage = driver.getCurrentUrl();
@@ -68,5 +84,20 @@ public class ShopPage {
         actualProductTableHead = driver.findElement(actualTextProductTableHead).getText();
         utils.callWaitPollingVisibility(thirtySeconds, fiveSeconds, actualTextProductTableHead);
         Assert.assertEquals(actualProductTableHead, expectedProductTableHead);
+    }
+
+    public void theUserAddsTheQuantityForAProduct() {
+        utils.callWaitPresenceElement(tenSeconds, firstProductText);
+        getFirstProductText = driver.findElement(firstProductText).getText();
+        System.out.println("The text of the first product: " + getFirstProductText);
+        Assert.assertEquals(getFirstProductText, expectedFirstProductText);
+        utils.callWaitPresenceElement(tenSeconds, getFirstProductQuantity);
+      //  String pageSource = driver.getPageSource();
+      //  System.out.println("L'elemento 'iphone X' è presente nel sorgente? " + pageSource.contains("iphone X"));
+        firstProductQuantity = driver.findElement(getFirstProductQuantity);
+        firstProductQuantity.clear();
+        firstProductQuantity.sendKeys(sendFirstFirstQuantity);
+        firstProductQuantity.getText();
+        System.out.println("First product quantity is: " + firstProductQuantity);
     }
 }
--- src/test/java/stepDefinitions/ShopSteps.java
+++ src/test/java/stepDefinitions/ShopSteps.java
@@ -28,4 +28,57 @@ public class ShopSteps {
     public void the_user_should_see_the_cart_page() {
         shopPage.theUserShouldSeeTheCartPage();
     }
+
+    @When("the user adds the quantity for a product")
+    public void the_user_adds_the_quantity_for_a_product() {
+        shopPage.theUserAddsTheQuantityForAProduct();
+    }
+
+
+    /*@And("checks the correctness of the prices")
+    public void checks_the_correctness_of_the_prices() {
+        // Gets the unit price of the first product and checks it with expected unit price
+        String actualUnitPriceFirstProduct = driver.findElement(By.xpath("//strong[text() = '₹. 100000']")).getText();
+        System.out.println("The actual price first product: " + actualUnitPriceFirstProduct);
+        Assert.assertEquals(actualUnitPriceFirstProduct, expectedUnitPriceFirstProduct);
+        // Gets the total price of the first product and checks it with expected total price
+        String actualTotalPriceFirstProduct = driver.findElement(By.xpath(("//strong[text() = '₹. 300000']"))).getText();
+        System.out.println("The actual total price first product: " + actualTotalPriceFirstProduct);
+        Assert.assertEquals(actualTotalPriceFirstProduct, expectedTotalPriceFirstProduct);
+        // Gets the quantity of the second product and checks it with expected quantity
+        String secondProductQuantity = driver.findElement(By.xpath("(//input[@id='exampleInputEmail1'])[2])")).getText();
+        System.out.println("The second product quantity: " + secondProductQuantity);
+        Assert.assertEquals(secondProductQuantity, expectedSecondProductQuantity);
+        // Gets the unit price of the second product and checks it with expected unit price
+        String actualUnitPriceSecondProduct = driver.findElement(By.xpath("(//strong[text() = '₹. 65000'])[1]")).getText();
+        System.out.println("The actual unit price second product: " + actualUnitPriceSecondProduct);
+        Assert.assertEquals(actualUnitPriceSecondProduct, expectedUnitPriceSecondProduct);
+        // Gets the total price of the second product and checks it with expected total price
+        String actualTotalPriceSecondProduct = driver.findElement(By.xpath("(//strong[text() = '₹. 65000'])[2]")).getText();
+        System.out.println("The actual total price of the second product: " + actualTotalPriceSecondProduct);
+        Assert.assertEquals(actualTotalPriceSecondProduct, expectedTotalPriceSecondProduct);
+        // Gets the total price and checks it with expected total price
+        String actualTotalPrice = driver.findElement(By.cssSelector("//strong[text() = '₹. 365000']")).getText();
+        System.out.println("The actual total price: " + actualTotalPrice);
+        Assert.assertEquals(actualTotalPrice, expectedTotalPrice);
+    }
+
+    @And("clicks on the checkout button")
+    public void clicks_on_the_checkout_button() {
+        // Gets the text checkout button and checks it with expected text
+        String actualCheckoutTextButton =  driver.findElement(By.cssSelector("button[class='btn btn-success']")).getText();
+        System.out.println("The text of the Checkout button: " + actualCheckoutTextButton);
+        Assert.assertEquals(actualCheckoutTextButton, expectedCheckoutTextButton);
+        // Clicks on the Checkout button
+        WebElement checkoutButon = driver.findElement(By.cssSelector("button[class='btn btn-success']"));
+        checkoutButon.click();
+    }
+
+    @Then("the user should see the checkout page")
+    public void the_user_should_see_the_checkout_page() {
+        // Gets the text delivery and checks it with expected text
+        String actualDeliveryText = driver.findElement(By.cssSelector("label[for='country']")).getText();
+        System.out.println("The delivery text: " + actualDeliveryText);
+        Assert.assertEquals(actualDeliveryText, expectedDeliveryText);
+    }*/
 }
