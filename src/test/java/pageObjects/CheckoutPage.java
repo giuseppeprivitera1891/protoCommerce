14d9d4c30a2831deed4600e99765670fba7b5c3d
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-02-04 10:58:55 +0100

    Created chooseDeliveryLocation on CheckoutPage.java

--- src/test/java/pageObjects/CheckoutPage.java
+++ src/test/java/pageObjects/CheckoutPage.java
@@ -1,4 +1,26 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utility.DriverManager;
+import utility.GenericUtils;
+
 public class CheckoutPage {
+    public WebDriver driver = DriverManager.getDriver();
+    public GenericUtils utils = new GenericUtils();
+
+    int fiveSeconds = 5;
+
+    WebElement deliveryLocation;
+
+    By location = By.id("country");
+    By selectCountry = By.xpath("(//div[@class='suggestions'])[1]");
+
+    public void chooseDeliveryLocation(String myDeliveryLocation) {
+        deliveryLocation = driver.findElement(location);
+        deliveryLocation.sendKeys(myDeliveryLocation);
+        utils.callWaitVisibility(fiveSeconds, selectCountry);
+        utils.performClick(selectCountry);
+    }
 }
