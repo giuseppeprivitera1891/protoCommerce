e920fff2467f24eb381b89c9d674e564e54b83a1
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-21 19:22:34 +0200

    Deleted Utils class and extended LoginPage.java and ShopPage.java classes with BaseTest.java

--- src/test/java/utility/BaseTest.java
+++ src/test/java/utility/BaseTest.java
@@ -3,16 +3,39 @@ package utility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class GenericUtils {
-    public WebDriver driver = DriverManager.getDriver();
+public class BaseTest {
+    public static WebDriver driver;
     WebDriverWait wait;
     WebElement clickable;
+
+    // Method to get the driver instance
+    public static void initDriver() {
+        if (driver == null) {
+            // Initialize the driver
+            driver = new FirefoxDriver();
+            // Basic configuration
+            driver.manage().window().maximize();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    // Method to close the driver
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 
     public void callWaitVisibility(int seconds, By locator) {
         wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
