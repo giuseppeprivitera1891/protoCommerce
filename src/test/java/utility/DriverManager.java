24b76a3e1dfee812e2da68bc185ab9cbc83ccae4
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-04-23 10:09:52 +0200

    Extended Utils.java with DriverManager.java

--- src/test/java/utility/DriverManager.java
+++ src/test/java/utility/DriverManager.java
@@ -36,32 +36,4 @@ public class DriverManager {
             driver = null;
         }
     }
-
-    public void callWaitVisibility(int seconds, By locator) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
-
-    public void callWaitInvisibility(int seconds, By locator) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
-    }
-
-    public void callWaitPollingVisibility(int seconds, int pollingEvery, By locator) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
-        wait.pollingEvery(Duration.ofSeconds(pollingEvery));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-    }
-
-    public void callWaitTitle(int seconds, String title) {
-        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
-        wait.until(ExpectedConditions.titleIs(title));
-    }
-
-    public void performClick(By locator) {
-        clickable = driver.findElement(locator);
-        new Actions(driver)
-                .click(clickable)
-                .perform();
-    }
 }
--- src/test/java/utility/Utils.java
+++ src/test/java/utility/Utils.java
@@ -1,4 +1,38 @@
 package utility;
 
-public class Utils {
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Utils extends DriverManager {
+    public void callWaitVisibility(int seconds, By locator) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void callWaitInvisibility(int seconds, By locator) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    public void callWaitPollingVisibility(int seconds, int pollingEvery, By locator) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.pollingEvery(Duration.ofSeconds(pollingEvery));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void callWaitTitle(int seconds, String title) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.titleIs(title));
+    }
+
+    public void performClick(By locator) {
+        clickable = driver.findElement(locator);
+        new Actions(driver)
+                .click(clickable)
+                .perform();
+    }
 }
